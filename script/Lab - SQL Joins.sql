339212d486e5dc145fbc7444c0cf3b6c1a0bfc3e
Ijeoma Osim <myhaijay@gmail.com>
2021-11-12 12:33:45 +0100

    Lab-SQL Joins - Week 7

---
+++ script/Lab - SQL Joins.sql
@@ -0,0 +1,27 @@
+-- 1. List number of films per category.
+select f.title, c.name from film as f
+join film_category as fc 
+on f.film_id = fc.film_id
+join category as c on c.category_id = fc.category_id;
+
+-- 2. Display the first and last names, as well as the address, of each staff member.
+select s.first_name, s.last_name, a.address from staff as s
+join address as a on a.address_id = s.address_id;
+
+-- 3. Display the total amount rung up by each staff member in August of 2005.
+select p.staff_id, sum(amount) as amount from payment as p
+join rental as r on r.staff_id = p.staff_id
+where r.rental_date like "2005-08%" 
+group by p.staff_id;
+
+-- 4. List each film and the number of actors who are listed for that film.
+select f.title, count(actor_id) as actors from film as f
+join film_actor as fa on f.film_id = fa.film_id
+-- join actor as a on fa.actor_id = a.actor_id
+group by title ;
+
+-- 5. Using the tables payment and customer and the JOIN command, list the total paid by each customer. List the customers alphabetically by last name.
+select c.first_name, c.last_name, sum(p.amount) as payment from payment as p
+join customer as c on p.customer_id = c.customer_id
+group by c.customer_id
+order by c.last_name;
